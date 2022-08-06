135494ebcfdafe8743366d6cd474021164aa3d14
Raj <rajkhulna@outlook.com>
2022-08-06 13:59:29 +0600

    Initial Commit

---
+++ GuessThe Flag/GuessThe_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessThe_FlagApp.swift
+//  GuessThe Flag
+//
+//  Created by Khalid Khan on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessThe_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
